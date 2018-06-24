e3e90f88ed8bf88fa357ca989e7478fb84b2935a
Joshua <jjlee2920@gmail.com>
2018-06-24 00:22:28 -0400

    readlogfile now invokes postToDiscord

--- postToDiscord.sh
+++ postToDiscord.sh
@@ -4,10 +4,18 @@
 # you may need to connect with a websocket the first time you run the bot
 #   use a library like discord.py to do so
 
+echo "$1"
+message1='{"content":'
+message2='}'
+
+message="$1"
+
+full_message=$message1"\"$message\""$message2
+echo $full_message
+
 curl -v \
 -H "Authorization: Bot TOKEN" \
 -H "User-Agent: myBotThing (http://some.url, v0.1)" \
 -H "Content-Type: application/json" \
--X POST \
--d '{"content":"Posting as a bot"}' \
+-d $full_message \
 https://discordapp.com/api/channels/CHANNELID/messages
--- readlogfile.sh
+++ readlogfile.sh
@@ -2,12 +2,12 @@
 
 string="!sr"
 
-tail -n 0 -F ~/Documents/PhantomBot-2.4.0.3/logs/chat/23-06-2018.txt | \
+tail -n 0 -F ~/Documents/PhantomBot-2.4.0.3/logs/chat/24-06-2018.txt | \
 while read LINE
 do
 echo "$LINE" | grep -q $string
 if [ $? = 0 ]
 then
-echo -e "$string found"
+./postToDiscord.sh "$string"
 fi
 done
