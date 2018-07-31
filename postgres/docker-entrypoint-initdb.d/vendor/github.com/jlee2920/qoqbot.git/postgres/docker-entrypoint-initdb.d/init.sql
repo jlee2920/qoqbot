7de2a731e24347449de6e3d4dc025630516e403b
Joshua <jjlee2920@gmail.com>
2018-07-31 19:03:42 -0400

    Remove vendor from postgres directory, vendor update

--- postgres/docker-entrypoint-initdb.d/vendor/github.com/jlee2920/qoqbot.git/postgres/docker-entrypoint-initdb.d/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE regulars(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    current_songs INT
-);
--- vendor/github.com/jlee2920/qoqbot.git/cmd/qoqbot/discordIRC.go
+++ vendor/github.com/jlee2920/qoqbot.git/cmd/qoqbot/discordIRC.go
@@ -6,16 +6,41 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	// _ "github.com/jinzhu/gorm/dialects/mssql"
-	// _ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	// _ "github.com/jinzhu/gorm/dialects/sqlite"
-
-	_ "github.com/lib/pq"
+	"github.com/bwmarrin/discordgo"
+	"github.com/jlee2920/qoqbot.git/config"
 )
 
+// Use of discordgo as an intro to discord IRC
+func startDiscordIRC() {
+
+	discord, _ := discordgo.New("Bot " + config.Config.DiscordToken)
+
+	discord.AddHandler(listenForMessage)
+	discord.AddHandler(readyForMessages)
+
+	_ = discord.Open()
+	defer discord.Close()
+
+}
+
+func readyForMessages(discord *discordgo.Session, ready *discordgo.Ready) {
+	_ = discord.UpdateStatus(0, "Qoqbot at your service!")
+	servers := discord.State.Guilds
+	fmt.Printf("Qoqbot has started on %d servers", len(servers))
+}
+
+func listenForMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	// Make sure to skip the id of qoqbot
+	if message.Author.ID == config.Config.DiscordBotID {
+		return
+	}
+	postToDiscord(fmt.Sprintf("Message sent by %s: %s", message.Author.Username, message.Message.Content))
+}
+
 // Takes the discordURL for where to POST and the discordToken of qoqbot to echo message
-func postToDiscord(discordURL, discordToken, message string) {
+func postToDiscord(message string) {
+	discordURL := config.Config.DiscordURL
+	discordToken := config.Config.DiscordToken
 	httpClient := &http.Client{}
 	contentBody := fmt.Sprintf(`{"content" : "%s"}`, message)
 	postingJSONStruct := []byte(contentBody)
